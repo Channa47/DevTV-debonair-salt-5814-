7528f75607597d44d01be8f37d264292f5388c5e
Channa47 <103635080+Channa47@users.noreply.github.com>
2022-12-14 11:28:26 +0530

    Changes

--- appletv/src/App.js
+++ appletv/src/App.js
@@ -1,19 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import { SignupPage } from './Pages/Signup';
-
-function App() {
-  return (
-    <SignupPage/>
- 
-
 import Navbar from './Componenets/Navbar';
-import Dummy from './Componenets/dummy';
+import MainRoutes from './Routes/mainRoutes';
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
+      <MainRoutes/>
     </div>
 
   );
