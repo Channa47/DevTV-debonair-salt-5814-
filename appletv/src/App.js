3a4285a1f70d9f49f80115ea314c3ba68f504f4e
Uddeshy Manoj Jaiswal <uddeshkrrish@gmail.com>
2022-12-15 21:19:41 +0530

    addedadminpage

--- appletv/src/App.js
+++ appletv/src/App.js
@@ -3,20 +3,13 @@ import './App.css';
 import { SignupPage } from './Pages/Signup';
 import Navbar from './Componenets/Navbar';
 import MainRoutes from './Routes/mainRoutes';
-import Example from './Pages/example';
-import Faq from './Componenets/Faq';
-import Slider from './Componenets/Slider';
 
 function App() {
-  // ()
   return (
     <div className="App">
       <Navbar/>
-      {/* <MainRoutes/> */}
-      {/* <Example/> */}
-      <Slider/>
+      <MainRoutes/>
     </div>
-
 
   );
 }
--- appletv/src/Pages/Adminpage.jsx
+++ appletv/src/Pages/Adminpage.jsx
@@ -3,51 +3,62 @@ import {
     FormControl,
     Input,
     Button
-  } from '@chakra-ui/react'
-import { useState } from 'react'
-import {useDispatch, useSelector} from "react-redux"
+} from '@chakra-ui/react'
+import { useState, useEffect } from 'react'
+import { useDispatch, useSelector } from "react-redux"
 
 import "./Signup.css"
 import { useForm } from 'react-hook-form'
-import { logindata} from '../redux/auth/Login/loginaction'
 
-const AdminPage = ()=>{
+import { getAdmindata } from '../redux/auth/admin/adminaction'
+
+const AdminPage = () => {
     const init = {
-        email:"",
-        password:"",
+        email: "",
+        password: "",
     }
-   const [data,setData] = useState(init)
+    const [data, setData] = useState(init)
 
-   const { handleSubmit, formState: { errors, isSubmitting },} = useForm()
-   const dispatch = useDispatch()
-  const user = useSelector(store=>store.getSignupreducer)
-  console.log(user)
-      const handleChange = (e)=>{
-            const {name,value} = e.target 
-            setData({...data, [name]:value})
-      }
-      const onSubmit = (data)=>{
-     
-        // dispatch(adminData(data)) 
-        
-      }
-    
-   return (<>
-      <form onSubmit={handleSubmit(()=>onSubmit(data))}>
-        <FormControl  className='form'  width="50%" height="90vh" paddingTop="3%" m="auto" marginTop="1%" paddingLeft="7%" paddingRight="7%">
+    const { handleSubmit, formState: { errors, isSubmitting }, } = useForm()
+    const dispatch = useDispatch()
+    useEffect(() => {
+        dispatch(getAdmindata())
+    }, [])
 
-            <Input name="email" value={data.email} onChange={handleChange} className='input'  type='email' placeholder="enter your email" />
+    const handleChange = (e) => {
+        const { name, value } = e.target
+        setData({ ...data, [name]: value })
+    }
+    const onSubmit = (data) => {
+        let adminuserdata = admin.filter(ele => ele.email === data.email && ele.password === data.password)
+      
+        if (adminuserdata.length > 0) {
+            alert(`Very Welcome sir ${adminuserdata[0].name}`)
+            localStorage.setItem("admintoken", JSON.stringify(adminuserdata[0].token))
+        }
+        else {
+            alert("you are not an admin")
+        }
 
-            <Input name="password" value={data.password} onChange={handleChange} className='input'  type='password' placeholder="Enter your password"/>  
+    }
 
-        </FormControl>
-        <Button mt={4} colorScheme='teal' isLoading={isSubmitting} type='submit'>
-        Submit
-      </Button>
-    
-     </form>
-</>
-   )      
+    return (<>
+        <form onSubmit={handleSubmit(() => onSubmit(data))}>
+            <h2>Admin's Login</h2>
+            <FormControl className='form' width="50%" height="90vh" paddingTop="3%" m="auto" marginTop="1%" paddingLeft="7%" paddingRight="7%">
+
+                <Input name="email" value={data.email} onChange={handleChange} className='input' type='email' placeholder="enter your email" />
+
+                <Input name="password" value={data.password} onChange={handleChange} className='input' type='password' placeholder="Enter your password" />
+
+            </FormControl>
+            <Button mt={4} colorScheme='teal' isLoading={isSubmitting} type='submit'>
+                Submit
+            </Button>
+
+        </form>
+    </>
+    )
 
 }
-export {AdminPage}
+export { AdminPage }
