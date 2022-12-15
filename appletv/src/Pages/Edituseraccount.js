8f9d597baecfcf3990b057842f79c1a8475ddc85
Uddeshy Manoj Jaiswal <uddeshkrrish@gmail.com>
2022-12-15 21:55:40 +0530

    addeduserdelete

---
+++ appletv/src/Pages/Edituseraccount.js
@@ -0,0 +1,52 @@
+
+import {
+    FormControl,
+    Input,
+    Button
+} from '@chakra-ui/react'
+import { useState } from 'react'
+import { useDispatch, useSelector } from "react-redux"
+
+import "./Signup.css"
+import { useForm } from 'react-hook-form'
+import { getdeleteuser } from '../redux/auth/admin/edituseraction'
+
+
+
+const Edituseraccount = () => {
+    const init = {
+        email: "",
+    }
+    const [data, setData] = useState(init)
+
+    const { handleSubmit, formState: { errors, isSubmitting }, } = useForm()
+    const dispatch = useDispatch()
+   
+
+    const handleChange = (e) => {
+        const { name, value } = e.target
+        setData({ ...data, [name]: value })
+    }
+    const onSubmit = (data) => {
+        dispatch(getdeleteuser(data))
+         
+    }
+
+    return (<>
+        <form onSubmit={handleSubmit(() => onSubmit(data))}>
+            <h2>Delete account</h2>
+            <FormControl className='form' width="50%" height="90vh" paddingTop="3%" m="auto" marginTop="1%" paddingLeft="7%" paddingRight="7%">
+
+                <Input name="email" value={data.email} onChange={handleChange} className='input' type='email' placeholder="enter your email" />
+
+            </FormControl>
+            <Button mt={4} colorScheme='teal' isLoading={isSubmitting} type='submit'>
+                Submit
+            </Button>
+
+        </form>
+    </>
+    )
+
+}
+export { Edituseraccount }
